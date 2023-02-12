2b9ec24450f09bafcd0c33309156413068e333ff
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-13 00:24:02 +0100

    Adding pages and nav components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>À propos</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. A consectetur
+        quibusdam tempore ad excepturi tempora sapiente amet laudantium nam
+        accusantium fugit iusto consequatur laboriosam voluptatum minus vel
+        cupiditate iure corrupti fugiat, adipisci molestiae repudiandae
+        aspernatur. Recusandae ipsa eveniet modi debitis molestias voluptas,
+        adipisci alias quasi nisi sint quidem temporibus eius obcaecati et
+        delectus corrupti aliquam sapiente laudantium nihil minima mollitia
+        voluptates! Officiis neque provident beatae odio sapiente iste nemo,
+        necessitatibus labore ducimus enim doloremque nisi veniam alias minima
+        rem incidunt praesentium nobis, assumenda recusandae similique
+        consequuntur ipsum repudiandae optio porro? Nesciunt, deleniti atque
+        consequuntur optio autem laborum, eum provident voluptatem vel esse
+        tenetur dicta architecto saepe, totam facilis debitis. Excepturi
+        architecto nihil veritatis aliquid, numquam asperiores. Voluptate
+        explicabo hic totam quae! Dicta, assumenda inventore nostrum soluta
+        voluptas nemo atque reprehenderit? Fuga illum deserunt dolores ipsum
+        quod saepe sunt cupiditate esse obcaecati quis. Est exercitationem ullam
+        fugit quod velit modi, deserunt provident, alias quas saepe hic? Beatae
+        odio minima quidem obcaecati dolores labore architecto quos repudiandae
+        odit ipsam alias voluptate illum possimus adipisci explicabo saepe vitae
+        debitis delectus officia totam vero, quisquam, maxime deleniti? Id sit
+        numquam ut expedita provident eos earum illum cupiditate et repudiandae
+        enim vero, dolore itaque. Beatae?
+      </p>
+    </div>
+  );
+};
+
+export default About;
