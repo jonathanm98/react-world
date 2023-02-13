16ff3b08bf7c46f8543d462c125b5ef5276f7071
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-02-13 01:26:34 +0100

    Added final sorting function and inputs

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,8 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(250);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
     axios
@@ -23,11 +25,30 @@ const Countries = () => {
           defaultValue={rangeValue}
           onChange={(e) => setRangeValue(e.target.value)}
         />
-      </ul>
-      <ul>
-        {data.slice(0, rangeValue).map((country) => (
-          <Card key={country.cca2} country={country} />
+        {radios.map((continent) => (
+          <li key={continent}>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={selectedRadio === continent}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>Reset continent</button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .slice(0, rangeValue)
+          .sort((a, b) => b.population - a.population)
+          .map((country) => (
+            <Card key={country.cca3} country={country} />
+          ))}
       </ul>
     </div>
   );
